32248122b4cb58aa7afded9a95ddd51f5774c5fe
sonal bagdiya <sonalbagdiya@gmail.com>
2019-09-02 15:46:07 +0530

    terraform

--- single-web-server/outputs.tf
+++ single-web-server/outputs.tf
@@ -2,3 +2,11 @@ output "public_ip" {
   value       = aws_instance.example.public_ip
   description = "The public IP of the web server"
 }
+
+output "vpc_id" {
+  value = "${aws_vpc.myvpc.id}"
+}
+
+output "subnet_id" {
+  value = "${aws_subnet.mysubnet.id}"
+}
--- single-web-server/vpc.tf
+++ single-web-server/vpc.tf
@@ -7,18 +7,10 @@ resource "aws_vpc" "myvpc" {
 }
 
 resource "aws_subnet" "mysubnet" {
-  vpc_id     = "${var.vpc_id}"
+  vpc_id     = "${aws_vpc.myvpc.id}"
   cidr_block = "10.20.1.0/24"
 
   tags {
     Name = "mysubnet"
   }
-}
-
-output "vpc_id" {
-  value = "${aws_vpc.myvpc.id}"
-}
-
-output "subnet_id" {
-  value = "${aws_subnet.mysubnet.id}"
 }
