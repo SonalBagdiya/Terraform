b9ef2793919fbb8e09be56e19c751506fa6edc8e
sonal bagdiya <sonalbagdiya@gmail.com>
2019-09-02 15:25:10 +0530

    terraform

--- single-web-server/vpc.tf
+++ single-web-server/vpc.tf
@@ -1,24 +1,24 @@
-resource "aws_vpc" "main" {
-  cidr_block  = "192.50.0.0/16"
+resource "aws_vpc" "myvpc" {
+  cidr_block    = "192.50.0.0/16"
   
-  tags {
-    Name = "Main"
+  tags = {
+    Name = "VPC-main"
   }
 }
 
-resource "aws_subnet" "main" {
+resource "aws_subnet" "mysubnet" {
   vpc_id     = "${var.vpc_id}"
   cidr_block = "10.20.1.0/24"
 
   tags {
-    Name = "Main"
+    Name = "mysubnet"
   }
 }
 
 output "vpc_id" {
-  value = "${aws_vpc.main.id}"
+  value = "${aws_vpc.myvpc.id}"
 }
 
 output "subnet_id" {
-  value = "${aws_subnet.main.id}"
+  value = "${aws_subnet.mysubnet.id}"
 }
